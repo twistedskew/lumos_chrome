7afb7fd5a0482e178be3b4704ae5dee79ef5e40d
Brian Ko <brian@lumoslabs.com>
2014-05-16 14:07:53 -0700

    using iframes instead

--- js/post-background.js
+++ js/post-background.js
@@ -1,4 +1,4 @@
-$(function() {
+$(function() {/*
   var xhr = new XMLHttpRequest();
   xhr.open('GET', 'http://static.sl.lumosity.com/compiled/trainer/lumosity_bridge-5bfbfa77a06a9de722bd79b2528e59a9.swf', true);
   xhr.responseType = 'blob';
@@ -7,17 +7,19 @@ $(function() {
     $obj.attr('data', window.URL.createObjectURL(this.response));
   };
 
-  xhr.send();
+  xhr.send();*/
 
   user = getUser();
 
   name = user.unique_name || user.email_address
 
-  $('#username').text('('+name+')');
+  $('#username').text(name);
 
   window['sendToJavaScript'] = function(data) {
     var eventName = data.shift();
     console.log('Received ' + eventName + ' with ' + data + ' from AS');
     //$t.trigger(eventName, data);
   };
+
+  $('#innerdiv').attr('src', 'http://lumosity.com/app/v4/games/' + games[Math.floor(Math.random()*games.length)]);
 });
