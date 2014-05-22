7ccf55b9c6b3460f6854d8f938127ca6f9f7bcc6
Abhishek Gupta <twistedskew@gmail.com>
2014-05-22 02:58:08 -0700

    update copy

--- js/post-background.js
+++ js/post-background.js
@@ -8,7 +8,7 @@ $(function() {
   if(checked_games.length>0){
     $('#innerdiv').attr('src', 'http://lumosity.com/app/v4/games/' + checked_games[Math.floor(Math.random()*checked_games.length)]);
   }else{
-    $('#outerdiv').append('<h3 style="color: red">Please select some games!</h3>');
+    $('#outerdiv').append('<h3 style="color: #dd1144; background:#fff; padding:30px; width:620px; height:460px; border: 1px solid #e2e2e2;">Please put at least 1 game in your break by clicking on the Lumosity app icon!</h3>');
   }
 
 });
