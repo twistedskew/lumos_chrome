5d6320885327e2652644f2b7736fa31340059fc9
Abhishek Gupta <twistedskew@gmail.com>
2014-06-13 15:34:13 -0700

    added splitting seeds game

--- js/background.js
+++ js/background.js
@@ -19,7 +19,8 @@ games = [
   'star-search',
   'train-of-thought',
   'tidal-treasures',
-  'trouble-brewing'
+  'trouble-brewing',
+  'splitting-seeds''
 ];
 
 games2 = [
@@ -102,5 +103,9 @@ games2 = [
   {
     slug: 'trouble-brewing',
     name: 'Trouble Brewing'
+  },
+  {
+    slug: 'splitting-seeds',
+    name: 'Splitting Seeds'
   }
 ];
