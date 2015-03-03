f48e7f281bbab0d7344bfd06419beb2c1adaf466
Alan Andrade <alan.andradec@gmail.com>
2015-03-03 00:19:10 -0800

    Refactors localStorage Access by wrapping it inside of lumos.Storage

--- js/popup.js
+++ js/popup.js
@@ -5,60 +5,78 @@ window.addEventListener('click',function(e){
 })
 
 function updateMenu (updateInformation) {
-  var selectedGames = JSON.parse(localStorage.checked)
+  var selectedGames = lumos.Storage.getAll()
     , value = updateInformation.value;
 
   switch (updateInformation.op) {
     case 'add':
       selectedGames.push(value)
       break;
+
     case 'remove':
       selectedGames = _(selectedGames).without(value)
       break;
+
     default:
       throw 'Unrecognized operation'
   }
-  localStorage.checked = JSON.stringify(selectedGames);
+
+  lumos.Storage.insert(selectedGames);
 }
 
-var checked_games;
+lumos.Storage = (function (ls, accessor) {
+  var get = function ()  { return ls[accessor] }
+    , set = function (v) { ls[accessor] = v }
 
-$(function (){
-  var $button = $('#schedule')
-    , $time   = $('#time');
+  // Boot with localStorage
+  if (!_.isString(get()))
+    insert(lumos.Repository.all('slug'))
 
-  if (_.isUndefined(localStorage.checked))
-    localStorage.checked = JSON.stringify(lumos.Repository.all('slug'));
+  // Keep a cached version of the content to skip serialization.
+  var cache = null
+    , list  = function () { return cache || JSON.parse(get()) }
+    , insert = function (object) {
+        set(JSON.stringify(cache = object));
+      };
 
-  checked_games = JSON.parse(localStorage.checked);
-
-  function schedule () {
-    var time = moment($time.val(), 'HH:mm');
-    if (time.isValid()) {
-      localStorage.setItem('time', time);
-      chrome.runtime.sendMessage(+time);
-      window.close();
-    } else {
-			$('#error').show();
-		}
+  return {
+    insert: insert,
+    getAll: list
   }
 
-  $button.on('click', schedule);
-  $time.keypress(function (e) {
-    if (e.which == 13) { schedule(); }
-  });
+})(localStorage, 'selectedGames');
+
+$(function (){
+  // Flaky Scheduling operations.
+  //
+  // var $button = $('#schedule')
+  // , $time   = $('#time');
+  // function schedule () {
+  //   var time = moment($time.val(), 'HH:mm');
+  //   if (time.isValid()) {
+  //     localStorage.setItem('time', time);
+  //     chrome.runtime.sendMessage(+time);
+  //     window.close();
+  //   } else {
+	// 		$('#error').show();
+	// 	}
+  // }
+  // $button.on('click', schedule);
+  // $time.keypress(function (e) {
+  //   if (e.which == 13) { schedule(); }
+  // });
 
   lumos.Repository.byPair(function (slug, name) {
     var $input = $('<input/>', { type: 'checkbox', id: slug }).addClass('game-name')
       , $label = $('<label/>', { for: slug }).text(name);
 
-    if (_(checked_games).contains(slug))
+    if (_(lumos.Storage.getAll()).contains(slug))
       $input.prop('checked', true);
 
     $('#games-list').append($input, $label);
   });
 
-  $( ".game-name" ).button().
+  $('.game-name').button().
     on('click', function(e) {
       var $target  = $(e.target)
         , gameSlug = $(e.target).attr('id')
