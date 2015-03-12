c0f05a31c6757b9055dbbd73ffc8dc5f1a3d75ce
Alan Andrade <alan.andradec@gmail.com>
2015-03-11 18:09:06 -0700

    [WIP] on management permissions

--- js/play/main.js
+++ js/play/main.js
@@ -9,4 +9,20 @@ define(['lumos/storage'], function (Storage) {
   }else{
     $('#outerdiv').append('<h3 style="color: #dd1144; background:#fff; padding:30px; width:620px; height:460px; border: 1px solid #e2e2e2;">Please put at least 1 game in your break by clicking on the Lumosity app icon!</h3>');
   }
+
+  chrome.management.getAll(function(extensions) {
+    var custom = _(extensions).filter(function (ext) {
+      return ext.type === 'extension';
+    });
+
+    custom.map(function (ext) {
+      console.log(ext.name);
+      console.log(ext.type);
+      console.log(ext.launchType);
+      console.log(ext.homepageUrl);
+      console.log(ext.permissions);
+      console.log(ext.optionsUrl);
+      console.log('------------');
+    });
+  });
 });
