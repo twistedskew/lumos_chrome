a2762f9f7f796ecbfa82326550b62f3fc5110c14
Jay O'Connor <joconnor@lumoslabs.com>
2016-07-15 10:23:22 -0700

    what is on production

---
+++ js/background.js
@@ -0,0 +1,141 @@
+games = [
+  'brain-shift-2',
+  'brain-shift-overdrive-2',
+  'chalkboard-challenge-2',
+  'color-match-2',
+  'disillusion-2',
+  'ebb-and-flow',
+  'follow-that-frog',
+  'lost-in-migration-2',
+  'memory-match-2',
+  'memory-match-overdrive',
+  'memory-matrix-2',
+  'penguin-pursuit-2',
+  'pet-detective',
+  'pinball-recall',
+  'raindrops-2',
+  'river-ranger',
+  'robot-factory',
+  'spatial-speed-match-2',
+  'speed-pack',
+  'speed-match-2',
+  'speed-match-overdrive',
+  'splitting-seeds',
+  'star-search',
+  'train-of-thought',
+  'tidal-treasures',
+  'trouble-brewing',
+  'word-bubbles-2'
+];
+
+games2 = [
+  {
+    slug: 'brain-shift-2',
+    name: 'Brain Shift'
+  },
+  {
+    slug: 'brain-shift-overdrive-2',
+    name: 'Brain Shift Overdrive'
+  },
+  {
+    slug: 'chalkboard-challenge-2',
+    name: 'Chalkboard Challenge'
+  },
+  {
+    slug: 'color-match-2',
+    name: 'Color Match'
+  },
+  {
+    slug: 'disillusion-2',
+    name: 'Disillusion'
+  },
+  {
+    slug: 'ebb-and-flow',
+    name: 'Ebb and Flow'
+  },
+  {
+    slug: 'follow-that-frog',
+    name: 'Follow that Frog'
+  },
+  {
+    slug: 'lost-in-migration-2',
+    name: 'Lost in Migration'
+  },
+  {
+    slug: 'memory-match-2',
+    name: 'Memory Match'
+  },
+  {
+    slug: 'memory-match-overdrive',
+    name: 'Memory Match Overdrive'
+  },
+  {
+    slug: 'memory-matrix-2',
+    name: 'Memory Matrix'
+  },
+  {
+    slug: 'penguin-pursuit-2',
+    name: 'Penguin Pursuit'
+  },
+  {
+    slug: 'pet-detective',
+    name: 'Pet Detective'
+  },
+  {
+    slug: 'pinball-recall',
+    name: 'Pinball Recall'
+  },
+  {
+    slug: 'raindrops-2',
+    name: 'Raindrops'
+  },
+  {
+    slug: 'river-ranger',
+    name: 'River Ranger'
+  },
+  {
+    slug: 'robot-factory',
+    name: 'Robot Factory'
+  },
+  {
+    slug: 'spatial-speed-match-2',
+    name: 'Spatial Speed Match'
+  },
+  {
+    slug: 'speed-pack',
+    name: 'Speed Pack'
+  },
+  {
+    slug: 'speed-match-2',
+    name: 'Speed Match'
+  },
+  {
+    slug: 'speed-match-overdrive',
+    name: 'Speed Match Overdrive'
+  },
+    {
+    slug: 'splitting-seeds',
+    name: 'Splitting Seeds'
+  },
+  {
+    slug: 'star-search',
+    name: 'Star Search'
+  },
+  {
+    slug: 'train-of-thought',
+    name: 'Train of Thought'
+  },
+  {
+    slug: 'tidal-treasures',
+    name: 'Tidal Treasures'
+  },
+  {
+    slug: 'trouble-brewing',
+    name: 'Trouble Brewing'
+  },
+  {
+    slug: 'word-bubbles-2',
+    name: 'Word Bubbles'
+  }
+
+];
